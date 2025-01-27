31221c2c1ded0b7ab5351529ce97da6f5f45c8b8
TylerSeverson <169163267+TylerSeverson@users.noreply.github.com>
2025-01-27 07:28:07 -0600

    Update upgrade_zabbix_proxy.sh

--- upgrade_zabbix_proxy.sh
+++ upgrade_zabbix_proxy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Zabbix Proxy Upgrade Script (6.4 to 7.0 on Ubuntu 22.04)
+# Zabbix Proxy Upgrade Script (6.4 to 7.0 on Ubuntu 24.04)
 
 # Stop the Zabbix proxy service
 systemctl stop zabbix-proxy
 
 # Clean up old downloaded files
 rm zabbix-release_*.deb
-
-#Set MySQL settings
-mysql -u root -e "SET GLOBAL log_bin_trust_function_creators = 1;"
 
 # Backup the current Zabbix proxy configuration
 cp /etc/zabbix/zabbix_proxy.conf /etc/zabbix/zabbix_proxy.conf.bak
@@ -18,28 +15,19 @@ cp /etc/zabbix/zabbix_proxy.conf /etc/zabbix/zabbix_proxy.conf.bak
 # This will prevent future accidental installations of 6.4 packages
 rm -Rf /etc/apt/sources.list.d/zabbix.list
 
-# Add the Zabbix repository for version 7.0
-wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu22.04_all.deb
-dpkg -i zabbix-release_7.0-1+ubuntu22.04_all.deb
+# Add the Zabbix repository for version 7.0 (Ubuntu 24.04)
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu24.04_all.deb
+dpkg -i zabbix-release_7.0-1+ubuntu24.04_all.deb
 apt update
 
 # Install the Zabbix proxy 7.0 package
-apt install --only-upgrade zabbix-proxy-mysql
-
-# Check the database schema version and upgrade if necessary
-if mysql -u root -e "SELECT schema_version FROM zabbix.dbconfig;" | grep -q "7.0"; then
-  echo "Database schema is already at 7.0. Skipping database upgrade."
-else
-  echo "Upgrading database schema..."
-  zabbix_db_upgrade -u root zabbix
-fi
+apt install --only-upgrade zabbix-proxy-sqlite3
 
 # Restore the backed-up configuration file. This is crucial as the new package might overwrite it.
 cp /etc/zabbix/zabbix_proxy.conf.bak /etc/zabbix/zabbix_proxy.conf
 
 # start the Zabbix proxy service
 systemctl start zabbix-proxy
-
 
 # Clean up downloaded files
 rm zabbix-release_*.deb
